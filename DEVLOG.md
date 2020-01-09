d3e100a0d26df1f260d527246dc8b2b0019650bd
Sharon Zou <szou00@stuy.edu>
2020-01-09 06:57:02 -0500

    more sdl

--- DEVLOG.md
+++ DEVLOG.md
@@ -1,4 +1,4 @@
-# battleship devlog
+## battleship devlog
 
 <b> December 27 2019 </b>
 - William + Sharon: Worked on proposal
@@ -8,3 +8,6 @@
 
 <b> January 7 2019 </b>
 - Sharon: Spent a long time trying to understand SDL but couldn't really understand all of it. Also had issues with fully downloading the library. Finally worked on just printing out the welcome screen and board.
+
+<b> January 8 2019 </b>
+- Sharon: Finally think I figured out SDL. Uploaded necessary folders that include the headers. Tested some code to create a window but am still unclear about some things.
---
+++ sdl_test.c
@@ -0,0 +1,38 @@
+//taken from https://dev.to/noah11012/using-sdl2-opening-a-window-79c
+//intended to do a basic check to fully see and understand how sdl works
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+int main()
+{
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+
+    if(!window)
+    {
+        std::cout << "Failed to create window\n";
+        return -1;
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if(!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+        return -1;
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000);
+}
